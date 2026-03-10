892868afdfe8f88b5f2216778b6e8744efe81d47
tomyroko <tomyroko@gmail.com>
2026-03-10 21:51:47 +0100

    Terminar de controlar los erores

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -24,27 +24,39 @@ export const getTaskByID = async (req, res) => {
 };
 
 export const createTask = async (req, res) => {
-  const task = new Task(req.body);
-  await task.save();
-  res.status(201).json(task);
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating task" });
+  }
 };
 
 export const updateTask = async (req, res) => {
-  const { id } = req.params;
-  const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
-    returnDocument: "after",
-  });
-  if (!updatedTask) {
-    return res.status(404).json({ error: "Task not found" });
+  try {
+    const { id } = req.params;
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
+      returnDocument: "after",
+    });
+    if (!updatedTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(400).json({ error: "Invalid task ID" });
   }
-  res.json(updatedTask);
 };
 
 export const deleteTask = async (req, res) => {
-  const { id } = req.params;
-  const deletedTask = await Task.findByIdAndDelete(id);
-  if (!deletedTask) {
-    return res.status(404).json({ error: "Task not found" });
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).json({ error: "Invalid task ID" });
   }
-  res.status(200).send();
 };
